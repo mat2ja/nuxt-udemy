c7bb8f590754d5a536ac77bc687824ea6df419ba
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-02 20:48:28 +0200

    nuxtServerInit

--- pages/index.vue
+++ pages/index.vue
@@ -13,58 +13,10 @@
 
 <script>
 import PostList from '@/components/posts/PostList.vue'
-import { nanoid } from 'nanoid'
 
 export default {
   components: {
     PostList
-  },
-  // ucitava se na serveru (zbog SEO), nema pristup .this, moze biti samo na page komponentama,
-  // ne pokazujuje loader nego blocka rutu sve dok nije obavljeno
-  //* it loads first on the server only if were loading the page for the first time or we refreshed it
-  async fetch (context) {
-    if (context.store.state.loadedPosts.length) {
-      return null
-    }
-    try {
-      const data = await new Promise((resolve, reject) => {
-        // eslint-disable-next-line nuxt/no-timing-in-fetch-data
-        setTimeout(() => {
-          resolve({
-            loadedPosts: [
-              {
-                id: nanoid(6),
-                title: 'Orchestrator Yen',
-                preview: 'Quod debitis consequatur accusamus.',
-                thumbnail: 'http://placekitten.com/601/400'
-              },
-              {
-                id: nanoid(6),
-                title: 'Grow Big',
-                preview: 'Voluptatibus nihil illo quia.',
-                thumbnail: 'http://placekitten.com/602/400'
-              },
-              {
-                id: nanoid(6),
-                title: 'Transmitter Brooks',
-                preview: 'Asperiores dicta aliquam.',
-                thumbnail: 'http://placekitten.com/603/400'
-              },
-              {
-                id: nanoid(6),
-                title: 'Brook Burg',
-                preview: 'Amet ipsam sint velit beatae optio.',
-                thumbnail: 'http://placekitten.com/606/400'
-              }
-            ]
-          })
-        }, 1000)
-      })
-      console.log('settng posts')
-      context.store.commit('setPosts', data.loadedPosts)
-    } catch (e) {
-      context.error(e)
-    }
   },
   computed: {
     loadedPosts () {
--- store/index.js
+++ store/index.js
@@ -1,3 +1,5 @@
+import { nanoid } from 'nanoid'
+
 export const state = () => ({
   loadedPosts: []
 })
@@ -15,6 +17,41 @@ export const mutations = {
 }
 
 export const actions = {
+  nuxtServerInit (vuexContext, context) {
+    console.log('vuexContext :>> ', vuexContext)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // eslint-disable-next-line no-unused-expressions
+        vuexContext.commit('setPosts', [
+          {
+            id: nanoid(6),
+            title: 'Orchestrator Yen',
+            preview: 'Quod debitis consequatur accusamus.',
+            thumbnail: 'http://placekitten.com/601/400'
+          },
+          {
+            id: nanoid(6),
+            title: 'Grow Big',
+            preview: 'Voluptatibus nihil illo quia.',
+            thumbnail: 'http://placekitten.com/602/400'
+          },
+          {
+            id: nanoid(6),
+            title: 'Transmitter Brooks',
+            preview: 'Asperiores dicta aliquam.',
+            thumbnail: 'http://placekitten.com/603/400'
+          },
+          {
+            id: nanoid(6),
+            title: 'Brook Burg',
+            preview: 'Amet ipsam sint velit beatae optio.',
+            thumbnail: 'http://placekitten.com/606/400'
+          }
+        ])
+        resolve()
+      }, 1000)
+    })
+  },
   setPosts ({ commit }, posts) {
     commit('setPosts', posts)
   }
